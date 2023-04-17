ae904365ed5253f27cf75018f3ce3cca464df3af
dev-carlson <carlsonroman03@gmail.com>
2023-04-18 01:33:35 +0800

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,42 +14,60 @@ function Footer() {
   };
 
   return (
-    <div className="terms-condition">
-      <h1>info</h1>
+    <>
+           <h1>info</h1>
       <p className="info">
-        Flagship Store location & hours Gammeltorv 6, 1st floor 1457 Copenhagen,
-        Denmark Weekdays - 10:00 - 18:00 Saturdays - 10:00 - 16:00
+        Flagship Store location & hours
       </p>
-      <textarea
-        className="text-area"
-        placeholder="Join our newsletter"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.keyCode === 13) {
-            e.preventDefault();
-          }
-        }}
-      ></textarea>
-      {text ? (
-        <button className="clear-button" type="submit" onClick={handleClear}>
-          Clear
-        </button>
-      ) : (
-        <button className="submit-button" type="submit" onClick={handleSubmit}>
-          Submit
-        </button>
-      )}
-      <form>
-        <label>
-          <input type="checkbox" required />I agree to the terms and conditions
-        </label>
-      </form>
-      <div className="footer-link">
-        <Link to="TermsAndCondition"> Terms</Link>
-        <Link to="CareAndRepair">Care & Repair</Link>
+      <p className="info">
+      Gammeltorv 6, 1st floor
+      </p>
+      <p className="info">
+      1457 Copenhagen, Denmark
+      </p>
+      <p className="info">
+      Weekdays - 10:00 - 18:00
+      </p>
+      <p className="info">
+      Saturdays - 10:00 - 16:00
+      </p>
+      <div className="terms-condition">
+        <textarea
+          className="text-area"
+          placeholder="Join our newsletter"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.keyCode === 13) {
+              e.preventDefault();
+            }
+          }}
+        ></textarea>
+        {text ? (
+          <button className="clear-button" type="submit" onClick={handleClear}>
+            Clear
+          </button>
+        ) : (
+          <button
+            className="submit-button"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        )}
+        <form>
+          <label>
+            <input type="checkbox" required />I agree to the terms and
+            conditions
+          </label>
+        </form>
+        <div className="footer-link">
+          <Link to="TermsAndCondition"> Terms</Link>
+          <Link to="CareAndRepair">Care & Repair</Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
