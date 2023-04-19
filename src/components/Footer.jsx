3d1b7d27c30c61ade9cb51e362a75f87f8e36af2
dev-carlson <carlsonroman03@gmail.com>
2023-04-20 03:09:08 +0800

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,10 @@ import React, { useState } from "react";
 import "../styles/Footer.css";
 import TermsAndCondition from "../routers/TermsAndCondition";
 import { Link } from "react-router-dom";
+
 function Footer() {
   const [text, setText] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
 
   const handleClear = () => {
     setText("");
@@ -11,55 +13,64 @@ function Footer() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (isChecked && text.includes("@")) {
+      alert("Submitted!");
+    } else if (!text.includes("@")) {
+      alert("Please enter a valid email address");
+    } else {
+      alert("Please agree to the terms and conditions.");
+    }
+  };
+
+  const handleCheckboxChange = (e) => {
+    setIsChecked(e.target.checked);
   };
 
   return (
     <>
-           <h1>info</h1>
-      <p className="info">
-        Flagship Store location & hours
-      </p>
-      <p className="info">
-      Gammeltorv 6, 1st floor
-      </p>
-      <p className="info">
-      1457 Copenhagen, Denmark
-      </p>
-      <p className="info">
-      Weekdays - 10:00 - 18:00
-      </p>
-      <p className="info">
-      Saturdays - 10:00 - 16:00
-      </p>
+      <h1>info</h1>
+      <p className="info">Flagship Store location & hours</p>
+      <p className="info">Gammeltorv 6, 1st floor</p>
+      <p className="info">1457 Copenhagen, Denmark</p>
+      <p className="info">Weekdays - 10:00 - 18:00</p>
+      <p className="info">Saturdays - 10:00 - 16:00</p>
       <div className="terms-condition">
-        <textarea
-          className="text-area"
-          placeholder="Join our newsletter"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.keyCode === 13) {
-              e.preventDefault();
-            }
-          }}
-        ></textarea>
-        {text ? (
-          <button className="clear-button" type="submit" onClick={handleClear}>
-            Clear
-          </button>
-        ) : (
-          <button
-            className="submit-button"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            Submit
-          </button>
-        )}
         <form>
+          <div>
+            <textarea
+              className="text-area"
+              placeholder="Join our newsletter"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.keyCode === 13) {
+                  e.preventDefault();
+                }
+              }}
+            ></textarea>
+            {isChecked ? (
+              <button
+                className="submit-button"
+                type="submit"
+                onClick={handleSubmit}
+                required
+              >
+                Submit
+              </button>
+            ) : (
+              <button
+                className="clear-button"
+                type="submit"
+                onClick={handleClear}
+              >
+                Clear
+              </button>
+            )}
+          </div>
+
           <label>
-            <input type="checkbox" required />I agree to the terms and
-            conditions
+            <input type="checkbox" onChange={handleCheckboxChange} required />I
+            agree to the terms and conditions
           </label>
         </form>
         <div className="footer-link">
