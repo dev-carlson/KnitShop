5292d98717744d6a5a36d85b35e47977dfbf40cf
dev-carlson <carlsonroman03@gmail.com>
2023-04-22 14:54:30 +0800

    added external -2

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-toastify'],
+      external: ["react-toastify/dist/ReactToastify.css"],
     },
   },
 });
