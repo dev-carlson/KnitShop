68bef3562313bdfa943192bfd4bf9adcfa576e94
dev-carlson <carlsonroman03@gmail.com>
2023-04-26 16:08:40 +0800

    update

---
+++ src/imageComponents/HomeComponents.jsx
@@ -0,0 +1,25 @@
+import dress1 from "../assets/dress1.png";
+import dress2 from "../assets/dress2.png";
+import dress3 from "../assets/dress3.png";
+import dress4 from "../assets/dress4.png";
+
+const HomeComponents = [
+  {
+    name: "Skipper Jacket",
+    image: dress1,
+  },
+  {
+    name: "Work Jacket",
+    image: dress2,
+  },
+  {
+    name: "Denim Jacket",
+    image: dress3,
+  },
+  {
+    name: "Leather Jacket",
+    image: dress4,
+  },
+];
+
+export default HomeComponents;
--- src/routers/Home.jsx
+++ src/routers/Home.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from "react";
 import "../styles/Home.css";
 import video1 from "../assets/video1.mp4";
-import dress1 from "../assets/dress1.png";
-import dress2 from "../assets/dress2.png";
-import dress3 from "../assets/dress3.png";
-import dress4 from "../assets/dress4.png";
 import vagi1 from "../assets/vegi1.jpg";
 import vagi2 from "../assets/vegi2.jpg";
 import vagi3 from "../assets/vegi3.jpg";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+
+import HomeComponents from "../imageComponents/HomeComponents";
 
 const Home = () => {
   const vegiImages = [vagi1, vagi2, vagi3];
@@ -32,28 +30,18 @@ const Home = () => {
           <source src={video1} />
         </video>
       </div>
-      <div className="content-container"></div>
       <div className="classic-item-header">
         <h1>CLASSIC ITEMS</h1>
       </div>
       <div className="classic-items">
-        <div className="item-container">
-          <img src={dress1} />
-          <p className="item-text">Skipper Jacket</p>
-        </div>
-        <div className="item-container">
-          <img src={dress2} />
-          <p className="item-text">Work Jacket</p>
-        </div>
-        <div className="item-container">
-          <img src={dress3} />
-          <p className="item-text">Beanie Medium</p>
-        </div>
-        <div className="item-container">
-          <img src={dress4} />
-          <p className="item-text">Seaman Trutleneck</p>
-        </div>
+        {HomeComponents.map((item, index) => (
+          <div key={index}>
+            <h2>{item.name}</h2>
+            <img src={item.image} alt={item.name} />
+          </div>
+        ))}
       </div>
+
       <div className="stories">
         <h1>MARINE</h1>
         <p>
@@ -76,7 +64,9 @@ const Home = () => {
               <button onClick={handlePrevClick}>
                 <AiOutlineArrowLeft />
               </button>
-              <span className="counter-image-carousel">{currentImageIndex + 1}/{numImages}</span>
+              <span className="counter-image-carousel">
+                {currentImageIndex + 1}/{numImages}
+              </span>
               <button onClick={handleNextClick}>
                 <AiOutlineArrowRight />
               </button>
