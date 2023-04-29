447708953d04edc18dc09228e50df471fce3b4fb
dev-carlson <carlsonroman03@gmail.com>
2023-04-29 12:41:24 +0800

    shop on-going

--- src/routers/Shop.jsx
+++ src/routers/Shop.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const Shop = () => {
   return (
-    <div>
-      Shop
-    </div>
-  )
-}
+    <>
+      <div className="shop-header">Shop</div>
+      <button className="filter-list">Signature Yarn</button>
+      <button className="filter-list">Organic Cotton</button>
+      <button className="filter-list">Undyed Yarn</button>
+      <button className="filter-list">Indigo Dyed Yarn</button>
+      <div className="navy-dress-images">
+        img
+      </div>
+    </>
+  );
+};
 
-export default Shop
+export default Shop;
